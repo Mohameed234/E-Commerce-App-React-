6de1abf05ef6e9a49da45f2ff98473fcd4708881
Mohamed234 <mohamedibrahimy911@gmail.com>
2026-02-01 10:33:30 +0200

    upload products slides

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
-import HeroSlider from '../../components/HeroSlider'
-import './home.css'
+import React, { useEffect, useState } from "react";
+import HeroSlider from "../../components/HeroSlider";
+import SlideProduct from "../../components/slideProducts/SlideProduct";
+import "./home.css";
 
+const categories = [
+  "smartphones",
+  "mobile-accessories",
+  "laptops",
+  "tablets",
+  "sports-accessories",
+];
 
 function Home() {
+  const [products, setProducts] = useState({});
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const results = await Promise.all(
+          categories.map(async (category) => {
+            const res = await fetch(
+              `https://dummyjson.com/products/category/${category}`,
+            );
+            const data = await res.json();
+            return { [category]: data.products };
+          }),
+        );
+
+        const productsData = Object.assign({}, ...results);
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error Fetching", error);
+      }finally{
+        setLoading(false)
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-        <HeroSlider />
+      <HeroSlider />
+
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        categories.map((category) => (
+          <SlideProduct
+            key={category}
+            data={products[category]}
+            title={category.replace("-", " ")}
+          />
+        ))
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
